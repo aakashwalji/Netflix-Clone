28b41ae1a831562aa314a4210ef76547c5196c32
Aakash Walji <aakashwalji@gmail.com>
2024-01-07 11:29:51 +0530

    added changes

--- main/clone.js
+++ main/clone.js
@@ -1,12 +1,12 @@
 let faqButtons = document.querySelectorAll(".faq-ques");
 for (let faqbtn of faqButtons){
     faqbtn.addEventListener("click",(e)=>{
+        e.target.classList.toggle("active");
         let ans = e.target.nextElementSibling;
-        if(!ans.classList.contains("show")){
-            ans.classList.add("show");
-        }
-        else{
-            ans.classList.remove("show");
-        }
+        if (ans.style.maxHeight) {
+            ans.style.maxHeight = null;
+          } else {
+            ans.style.maxHeight = ans.scrollHeight + "px";
+          } 
     })
 };
